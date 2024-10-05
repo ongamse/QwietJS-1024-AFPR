91df12bcef74eb8b83727fdfba55d48ef32a3d0e
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-10-04 21:57:26 -0400

    Fixing src/Controllers/ImageLookup.js for finding 1

--- src/Controllers/ImageLookup.js
+++ src/Controllers/ImageLookup.js
@@ -2,14 +2,22 @@ const fs = require("fs");
 const { logger } = require("../Logger");
 
 class ImageLookup {
-  get(req, res) {
-    /* File Traversal exploit */
-    /* Can read any file in the server by passing the filename (image) in the query params */
-    /* ex: http GET http://localhost:8089/api/v1/image-lookup image=="package.json" */
-    const fileContent = fs.readFileSync(req.query.image).toString();
-    logger.debug(fileContent);
+get(req, res) {
+  /* File Traversal exploit */
+  /* Can read any file in the server by passing the filename (image) in the query params */
+  /* ex: http GET http://localhost:8089/api/v1/image-lookup image=="package.json" */
+  let sanitizedFileName = req.query.image.replace(/[^a-z0-9\\.]+/gi, ''); // sanitize file name
+  try {
+    const fileContent = fs.readFileSync(`./uploads/${sanitizedFileName}`).toString();
+    // logger.debug(fileContent); // commented out due to potential exposure of sensitive info
     res.send(fileContent);
+  } catch (error) {
+    logger.error(error);
+    res.status(500).send('An error occurred while trying to read the file.');
   }
 }
 
+}
+
 module.exports = ImageLookup;
+
